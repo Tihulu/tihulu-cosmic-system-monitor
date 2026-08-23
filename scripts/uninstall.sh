abd96dd277d53e0b72fde5210e26e68757273470
Tihulu <40742160+Tihulu@users.noreply.github.com>
2026-08-23 05:28:48 +0200

    Add uninstall script

---
+++ scripts/uninstall.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+PREFIX="${PREFIX:-/usr}"
+APP_ID="io.github.tihulu.SystemMonitor"
+
+sudo rm -f \
+  "$PREFIX/bin/tihulu-cosmic-system-monitor" \
+  "$PREFIX/share/applications/$APP_ID.desktop" \
+  "$PREFIX/share/metainfo/$APP_ID.metainfo.xml" \
+  "$PREFIX/share/icons/hicolor/scalable/apps/$APP_ID.svg"
+
+echo "Tihulu System Monitor removed."
